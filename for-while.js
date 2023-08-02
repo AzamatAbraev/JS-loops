12264971d0fdf6355ca01cfb0833c0583bb8ca8f
AzamatAbraev <azamatabraev07@gmail.com>
2023-08-02 11:58:07 +0500

    JS Loops

---
+++ for-while.js
@@ -0,0 +1,187 @@
+//* ----- For1 ----- *//
+
+// let a = 3;
+// let b = 10;
+
+// let count = 0;
+
+// for (let i = a; i <= b; i++) {
+//   console.log(i);
+//   count++;
+// }
+
+// console.log("Raqamlar soni =", count);
+
+//* ----- For2 ----- *//
+
+// let a = 10;
+// let b = 5;
+
+// let count = 0;
+
+// for (let i = a; i >= b; i--) {
+//   console.log(i);
+//   count++;
+// }
+
+// console.log("Raqamlar soni =", count);
+
+//* ----- For3 ----- *//
+
+// let price = 2000;
+
+// for (let i = 1; i <= 10; i++) {
+//   console.log(i * price);
+// }
+
+//* ----- For4 ----- *//
+
+// let price = 3000;
+
+// for (let i = 12; i <= 20; i++) {
+//   if (i % 2 == 0) {
+//     console.log((i * price) / 10);
+//   }
+// }
+
+// for (let i = 12; i <= 20; i += 2) {
+//   console.log((i * price) / 10);
+// }
+
+//* ----- For5 ----- *//
+
+// let a = 1;
+// let b = 11;
+
+// let sum = 0;
+
+// for (let i = a; i <= b; i++) {
+//   sum += i;
+// }
+
+//   console.log("Raqamlar yig`indisi =", sum);
+
+//* ----- For6 ----- *//
+
+// let a = 1;
+// let b = 3;
+
+// let mul = 1;
+
+// for (let i = a; i <= b; i++) {
+//   mul *= i;
+// }
+
+//   console.log("Raqamlar ko'paytmasi =", mul);
+
+//* ----- For7 ----- *//
+
+// let a = 4;
+// let b = 5;
+
+// let sum = 0;
+
+// for (let i = a; i <= b; i ++) {
+//   sum += i ** 2;
+// }
+
+//   console.log("Raqamlar kvadratlari yig`indisi =", sum);
+
+//* ----- For8 ----- *//
+
+// let n = 2;
+
+// let S = 0;
+
+// for (let i = 1; i <= n; i++) {
+//   let x = (1 / i);
+//   S += x;
+// }
+
+//   console.log("Yig'indi =", S);
+
+//* ----- For9 ----- *//
+
+// let n = 10;
+
+// let S = 1;
+
+// for (let i = 11; i <= n * 10; i++) {
+//   let x = i / 10;
+//   S *= x;
+// }
+
+// console.log("Ko`paytma =", S);
+
+
+
+//* ----- For10 ----- *//
+
+// let n = 10;
+// let m = 0;
+// let sum = 0;
+
+// for (let i = 1; i <= n; i++) {
+//   let x = (i * 2) - 1;
+//   m += x;
+//   console.log(m);
+//   sum += x;
+// }
+
+// console.log("n kvadrat =", sum);
+
+
+// * ----- For11 ----- *//
+
+// let n = 2;
+// let a = 3;
+// let mul = 1;
+
+// for (let i = 1; i <= n; i++) {
+//   mul *= a;
+// }
+
+// console.log("Natija =", mul);
+
+
+//* ----- For12 ----- *//
+
+// let n = 3;
+// let a = 5;
+// let mul = 1;
+
+// for (let i = 1; i <= n; i++) {
+//   mul *= a;
+//   console.log(mul);
+// }
+
+
+//* ----- For13 ----- *//
+
+// let n = 3;
+// let a = 5;
+// let mul = 1;
+// let sum = 1;
+
+// for (let i = 1; i <= n; i++) {
+//   mul *= a;
+//   console.log(mul);
+//   sum += mul;
+// }
+
+// console.log("Yig'indi =", sum);
+
+
+//* ----- For14 ----- *//
+
+// let n = 3;
+// let mul = 1;
+// let S =  0;
+// let x = 1;
+
+// for (let i = 1; i <= n; i++) {
+//   for (let j = 1; j <= i; j++) {
+//     console.log(j);
+//   }
+// }
+
